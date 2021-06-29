ed70b9ed237a40b88a72b185f1a159252fa55b05
ssongd-creator <https://github.com/ssongd-creater/test_0524.git>
2021-06-29 23:49:59 +0900

    6/29 slide 재구현 중

--- js/index.js
+++ js/index.js
@@ -1,50 +1,119 @@
-const slideBox = document.querySelector('.banner_container');
-let slider = document.querySelector('.main-slide');
-const slide = document.querySelectorAll('.slide');
-const slidelength = slide.length;
-const prevBtn = document.querySelector('#banner_prev');
-const nextBtn = document.querySelector('#banner_next');
-const mainSlide = document.querySelector(".banner_container .main-slide");
 
-let slideImg = 0;
+  let current = 0;
+  const btn=document.querySelectorAll('.btns button');
+  const slideBoxes=document.querySelector('.main-slide');
+  const slideBox = document.querySelectorAll('.slide');
 
-// function slideImgbox(n) {
-//   slideImg = n;
-// }
+  console.log(slideBox);
+
+  function slider(n){
+    //1차 시도
+    //  slideBoxes.animate(
+    //    [{ left: -100 *n+ "%" }]
+    //    ),{
+    //      duration:300,
+    //      iterations:1,
+    //      fill:"forward"
+    //    }
+
+    //2차 시도
+    // slideBoxes.style.left = -100 * n + "%";
+
+    //유명한 플러그인 slick slider, light slider, owl carousel
+    
+    slideBoxes.animate(
+       [{ transform:'translateX('+(-100 * n) +'%)'}]
+       ,{
+         duration:300,
+         iterations:1,
+         fill:"forwards"
+       });
+     current = n;
+  }
+
+  btn.forEach(function(elm){
+    //console.log(elm);
+    elm.addEventListener('click', function () {
+      console.log(this); //this=elm
+      if(this.classList.contains('prev')){
+        //console.log("true")
+        if(current == 0){
+          return false
+        }else{
+          slider(current -1);
+        };
+      }else{
+        if(current == slideBox.length-1){
+          return false
+        }else{
+          slider(current+1);
+        };
+      }
+      //slideBoxes.animate({left:-100 + "%"},500);
+      
+    });
+
+  });
+
+  // btn.addEventListener('click', function(){
+  //   //slideBoxes.animate({left:-100 + "%"},500);
+  //   if(current == slideBox.length - 1){
+  //     return false
+  //   }else{
+  //     slider(current+1);
+  //   };
+  //   console.log(this);
+  // });
+
+  slider(current);
+
+// const slideBox = document.querySelector('.banner_container');
+// let slider = document.querySelector('.main-slide');
+// const slide = document.querySelectorAll('.slide');
+// const slidelength = slide.length;
+// const prevBtn = document.querySelector('#banner_prev');
+// const nextBtn = document.querySelector('#banner_next');
+// const mainSlide = document.querySelector(".banner_container .main-slide");
+
+// let slideImg = 0;
+
+// // function slideImgbox(n) {
+// //   slideImg = n;
+// // }
 
 
-prevBtn.addEventListener('click', function () {
-  //alert("hi");
-  slider = 0;
-  slideImg--;
-  slider = slideImg * -100 + '%';
-  // console.log(slider);
-  mainSlide.style.left = slider;
-});
+// prevBtn.addEventListener('click', function () {
+//   //alert("hi");
+//   slider = 0;
+//   slideImg--;
+//   slider = slideImg * -100 + '%';
+//   // console.log(slider);
+//   mainSlide.style.left = slider;
+// });
 
-nextBtn.addEventListener('click', function () {
-  slider = 0;
-  slideImg++
-  slider = slideImg * -100 + '%';
-   //console.log(slider);
-  mainSlide.style.left = slider;
-});
+// nextBtn.addEventListener('click', function () {
+//   slider = 0;
+//   slideImg++
+//   slider = slideImg * -100 + '%';
+//    //console.log(slider);
+//   mainSlide.style.left = slider;
+// });
 
-//console.log(slider);
+// //console.log(slider);
 
-function removeBtn() {
-  //let removeBtn = document.querySelector(".banner_container .main-slide");
-  //removeBtn = 1;
-  const abc = abc.length;
+// function removeBtn() {
+//   //let removeBtn = document.querySelector(".banner_container .main-slide");
+//   //removeBtn = 1;
+//   const abc = abc.length;
   
-  if (mainSlide.style.left == 0) {
-    prevBtn.classList.add('disabled');
-  }else{
-    prevBtn.classList.remove('disabled');
-  };
-};
+//   if (mainSlide.style.left == 0) {
+//     prevBtn.classList.add('disabled');
+//   }else{
+//     prevBtn.classList.remove('disabled');
+//   };
+// };
 
-removeBtn();
+// removeBtn();
 
 //------------------
 
