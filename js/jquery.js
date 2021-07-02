360b135fb22dbaad2bf38fa69c0f4127d9924ee9
ssongd-creater <sksk1440@gmail.com>
2021-07-02 18:31:10 +0900

    7/2 media css 510px까지 완료

---
+++ js/jquery.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('.slide-gif').slick({
+    fade:true,
+    slide:'div',
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll:1,
+    adaptiveHeight: true
+  });
+
+});
